7157e12a2752cc6894d00228b87d45d7533fcdfc
Alexander-Ilyin3 <Alexander.ilyin3@gmail.com>
2020-01-03 20:43:03 +0200

    remove useless js files

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// let pic = document.createElement( 'img' )
-//     pic.setAttribute( 'style', 'width: 45px, height: 30px' )
-//     pic.setAttribute( 'src', '../pic/dialog-shrinked.png' )
-//     document.querySelector('#gsap').appendChild(pic)
-
-
-
-// gsap.to(pic, {duration: 2, x:300})
